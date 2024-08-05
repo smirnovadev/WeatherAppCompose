6724604e8292b25dfee7bec017b928599349c7f4
smirnovadev <annaparfenova514@gmail.com>
2024-08-05 23:35:43 +0300

    commit test

---
+++ app/src/main/java/com/example/weatherappcompose/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.example.weatherappcompose
+
+import android.content.Context
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.weatherappcompose.ui.theme.WeatherAppComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WeatherAppComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    val state = remember {
+        mutableStateOf("unknow")
+    }
+    Column(modifier = Modifier.fillMaxSize()) {
+        Box(modifier = Modifier.fillMaxHeight(0.5f)
+            .fillMaxWidth(), contentAlignment = Alignment.Center
+        ){
+            Text(text = "Temp in  $name = ${state.value}")
+        }
+        Box(modifier = Modifier.fillMaxHeight()
+            .fillMaxWidth(),
+            contentAlignment = Alignment.BottomCenter
+        ) {
+            Button(onClick = {
+                             getResult()
+            }, modifier = Modifier.padding(5.dp).fillMaxWidth()) {
+                Text(text = "Refresh")
+            }
+
+        }
+    }
+}
+
+private fun getResult(city: String, state: MutableState<String>, context: Context) {
+
+}
